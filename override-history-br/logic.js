4eee75fe6c4fef6c84eecc6c7c502a1390922994
王露 <wanglu@mininglamp.com>
2018-06-23 00:17:53 +0800

    add up and down and fourth split page

--- override-history-br/logic.js
+++ override-history-br/logic.js
@@ -1,6 +1,7 @@
 let $ = document.getElementById.bind(document);
 
 function constructHistory(historyItems) {
+  console.log(historyItems);
   let template = $('historyTemplate');
   for (let item of historyItems) {
     let pageName = template.content.querySelector(".historyWrap");
@@ -9,11 +10,13 @@ function constructHistory(historyItems) {
     if (item.title === '') {
       pageName.innerHTML = host;
     }
+    document.getElementById('app').innerHTML += item.title;
   }
+
 }
 
 chrome.history.search({
-  text: '',
+  text: 'google',
   startTime: (new Date).getTime() - 1000 * 60 * 60 * 24 * 7,
   maxResults: 99
 }, constructHistory);
--- resize-window/background.js
+++ resize-window/background.js
@@ -1,19 +1,58 @@
 var maxWidth = window.screen.availWidth;
 var maxHeight = window.screen.availHeight;
 var halfLeft = {
-    left: 0,
-    top: 0,
-    width: maxWidth / 2,
-    height:  maxHeight
+  left: 0,
+  top: 0,
+  width: parseInt(maxWidth / 2),
+  height:  maxHeight
 };
 
 var halfRight = {
-    left: maxWidth / 2,
-    top: 0,
-    width: maxWidth / 2,
-    height:  maxHeight
+  left:  parseInt(maxWidth / 2),
+  top: 0,
+  width:  parseInt(maxWidth / 2),
+  height:  maxHeight
 };
 
+var halfTop = {
+  top: 0,
+  left: 0,
+  width: maxWidth,
+  height: parseInt(maxHeight / 2)
+};
+
+var halfBottom = {
+  top: parseInt(maxHeight / 2),
+  left: 0,
+  width: maxWidth,
+  height: parseInt(maxHeight / 2)
+}
+
+var fouth1 = {
+  top: 0,
+  left: 0,
+  width:  parseInt(maxWidth / 2),
+  height: parseInt(maxHeight / 2)
+}
+
+var fouth2 = {
+  top: 0,
+  left: parseInt(maxHeight / 2),
+  width:  parseInt(maxWidth / 2),
+  height: parseInt(maxHeight / 2)
+}
+var fouth3 = {
+  top: parseInt(maxHeight / 2),
+  left: 0,
+  width:  parseInt(maxWidth / 2),
+  height: parseInt(maxHeight / 2)
+}
+var fouth4 = {
+  top: parseInt(maxHeight / 2),
+  left: parseInt(maxHeight / 2),
+  width:  parseInt(maxWidth / 2),
+  height: parseInt(maxHeight / 2)
+}
 document.getElementById('two').addEventListener('click', function(){
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
       chrome.windows.update(tabs[0].windowId, halfLeft);
@@ -24,6 +63,34 @@ document.getElementById('two').addEventListener('click', function(){
 
   });
 })
+document.getElementById('up').addEventListener('click', function(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+      chrome.windows.update(tabs[0].windowId, halfTop);
+
+      chrome.windows.create({tabId: tabs[0].id}, function(win){
+          chrome.windows.update(win.id, halfBottom);
+      })
+
+  });
+})
+document.getElementById('fouth').addEventListener('click', function(){
+  chrome.tabs.query({status: "complete"}, function(tabs){
+      chrome.windows.update(tabs[0].windowId, fouth1);
+      chrome.windows.create({tabId: tabs[0].id}, function(win){
+          chrome.windows.update(win.id, fouth1);
+      })
+      chrome.windows.create({tabId: tabs[1].id}, function(win){
+          chrome.windows.update(win.id, fouth2);
+      })
+      chrome.windows.create({tabId: tabs[2].id}, function(win){
+          chrome.windows.update(win.id, fouth3);
+      })
+      chrome.windows.create({tabId: tabs[3].id}, function(win){
+          chrome.windows.update(win.id, fouth4);
+      })
+  });
+})
 chrome.windows.onCreated.addListener(function (win) {
-  chrome.windows.update(win.id, halfRight);
+  debugger
+  chrome.windows.update(win.id, fouth1);
 })
